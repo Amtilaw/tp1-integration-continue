1dd8cfd2e1760c73cdf14cc7dfe4328d5276a4ef
Jeanjean <adrien.jeanjean@epsi.fr>
2022-10-27 14:45:22 +0200

    New realease

--- src/main/java/cicd/learn/tp1/App.java
+++ src/main/java/cicd/learn/tp1/App.java
@@ -7,10 +7,6 @@ package cicd.learn.tp1;
 public class App {
     public static void main(String[] args) {
         System.out.println("CICD rocks!");
-<<<<<<< HEAD
-        System.out.println("FEATURE #2");
-=======
         System.out.println("FEATURE v3");
->>>>>>> feature/#003
     }
 }
