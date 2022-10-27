40a3a9917a4ddba8646a2514689c5244da2faef5
Jeanjean <adrien.jeanjean@epsi.fr>
2022-10-27 14:36:19 +0200

    New feature 3

--- src/main/java/cicd/learn/tp1/App.java
+++ src/main/java/cicd/learn/tp1/App.java
@@ -7,6 +7,6 @@ package cicd.learn.tp1;
 public class App {
     public static void main(String[] args) {
         System.out.println("CICD rocks!");
-        System.out.println("FEATURE");
+        System.out.println("FEATURE v3");
     }
 }
